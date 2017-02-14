98e156a28ffabb2982437b0ffde1d80fd81b4864
Jongbeom Jeon <Jongbeom Jeon>
2017-02-14 12:01:15 -0500

    test2

--- app/src/main/java/com/example/jongbeom/cs2340_m4/EndScreen.java
+++ app/src/main/java/com/example/jongbeom/cs2340_m4/EndScreen.java
@@ -11,5 +11,19 @@ public class EndScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
 
+    Thread myThread = new Thread() {
+        @Override
+        public void run() {
+            try {
+                sleep(4000);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+    myThread.start();
     }
 }
