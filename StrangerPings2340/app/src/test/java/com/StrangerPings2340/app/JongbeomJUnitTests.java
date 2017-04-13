045ee08db969528550f946166eb6c9dba9dbd323
jjeon49 <jjeon49@gatech.edu>
2017-04-12 22:42:37 -0400

    Update JongbeomJUnitTests.java

--- StrangerPings2340/app/src/test/java/com/StrangerPings2340/app/JongbeomJUnitTests.java
+++ StrangerPings2340/app/src/test/java/com/StrangerPings2340/app/JongbeomJUnitTests.java
@@ -1,11 +1,9 @@
 package com.StrangerPings2340.app;
 
-
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 /**
  * JUnitTests created for M10 by Jongbeom
@@ -15,27 +13,30 @@ import static junit.framework.Assert.assertTrue;
  *
  *
  */
-
 public class JongbeomJUnitTests {
     @Test
-    public void testReportToString() {
-        WaterPurityReport purityReport1 = null;
-        WaterPurityReport purityReport2 = new WaterPurityReport();
-        purityReport1 = new WaterPurityReport();
-        purityReport1.setAddressString("1000 Atlantic DR NW, Atlantc, GA 30318");
-        purityReport1.setReportNumber(1);
-        purityReport1.setTimestamp(100000);
-        purityReport1.setName("Water_Purity_Report_Test");
-        purityReport1.setWaterCondition("Potable");
-        purityReport1.setContaminantPPM(100);
-        purityReport1.setVirusPPM(10);
+    public void testUserTypeToString() throws Exception {
 
-        assertEquals("Report cannot copy name correctly", purityReport2.getName(),purityReport1.getName());
-        assertEquals("Report cannot copy address string correctly", purityReport2.getAddressString(),purityReport1.getAddressString());
-        assertEquals("Report cannot copy report number correctly", purityReport2.getReportNumber(),purityReport1.getReportNumber());
-        assertEquals("Report cannot copy timestamp correctly", purityReport2.getTimestamp(),purityReport1.getTimestamp());
-        assertEquals("Report cannot copy water condition correctly", purityReport2.getWaterCondition(),purityReport1.getWaterCondition());
-        assertEquals("Report cannot copy water virus PPM correctly", purityReport2.getVirusPPM(),purityReport1.getVirusPPM());
-        assertEquals("Report cannot copy water contamination PPM correctly", purityReport2.getContaminantPPM(),purityReport1.getContaminantPPM());
+        UserType test_UserType = UserType.USER;
+        String expectedString = "USER";
+        assertEquals("User Type Test",expectedString, UserType.userTypeToString(test_UserType));
+
+        test_UserType = UserType.WORKER;
+        expectedString = "WORKER";
+        assertEquals("Worker Type Test",expectedString, UserType.userTypeToString(test_UserType));
+
+        test_UserType = UserType.MANAGER;
+        expectedString = "MANAGER";
+        assertEquals("Manager Type Test",expectedString, UserType.userTypeToString(test_UserType));
+
+        test_UserType = UserType.ADMIN;
+        expectedString = "ADMIN";
+        assertEquals("Admin Type Test",expectedString, UserType.userTypeToString(test_UserType));
+
+        test_UserType = null;
+        assertNull("Null Test", UserType.userTypeToString(test_UserType));
+
+        expectedString = "ABC";
+        assertNull("Default Test", WaterType.stringToWaterType(expectedString));
     }
 }
